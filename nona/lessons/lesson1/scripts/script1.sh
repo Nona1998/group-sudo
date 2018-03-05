18442cb5427aec694b8582cb4b8c8d6a2e6551f9
Nona Musaelyan <musaielian.nona@mail.ru>
2018-03-05 22:17:01 +0400

    Added Nona

---
+++ nona/lessons/lesson1/scripts/script1.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cd /workspaces
+cd lessons
+cd lesson1
+cd output
+mkdir u1
+cd u1
+mkdir u2
+mkdir u3
+touch a.txt
+cd u2
+touch a.txt
+touch b.txt
---
+++ nona/lessons/lesson1/scripts/script2.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+cd /workspaces/lessons/lesson1/output/u1
+echo "u1:a.txt:"
+cat /workspaces/lessons/lesson1/output/u1/a.txt
+cd /workspaces/lessons/lesson1/output/u1/u2 
+echo "u2:a.txt:" 
+cat /workspaces/lessons/lesson1/output/u1/u2/a.txt 
+echo "u2:b.txt:" 
+cat /workspaces/lessons/lesson1/output/u1/u2/b.txt
+

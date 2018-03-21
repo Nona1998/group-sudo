eb64fc1212c8544bcc6026873e1cbe108e8ff2a2
Nona1998 <musaielian.nona@mail.ru>
2018-03-21 14:14:35 +0400

    Added conversation.cpp and shop.cpp

--- nona/lessons/lesson6/conversation.cpp
+++ nona/lessons/lesson6/conversation.cpp
@@ -5,28 +5,28 @@ int main() {
         string name1;
 	std::cout <<"What is your name? --> ";
 	std::cin >>name1;
-	std::cout <<"My name is " << name1 << std::endl;
+	std::cout <<"My name is " << name1 << "." << std::endl;
 	int age1;
 	std::cout <<"How old are you? --> ";
 	std::cin >>age1;
-	std::cout <<"My age is " << age1 << std::endl;
+	std::cout <<"I am " << age1 << " years old." << std::endl;
+	std::cout <<"That's enough about you!"<<std::endl;
 	string name2;
 	std::cout <<"What is your friend's name? --> ";
 	std::cin >>name2;
-	std::cout <<"My friend's name is " << name2 << std::endl;
+	std::cout <<"My friend's name is " << name2 << "." << std::endl;
 	int age2;
 	std::cout <<"How old is your friend?  --> ";
 	std::cin >>age2; 
-	std::cout <<"My friend's age is " << age2 << std::endl;
+	std::cout <<"My friend is " << age2 << " years old." << std::endl;
 	if (age1==age2) {
-		std::cout << name2 << " is the same age as " << name1 << std::endl;
+		std::cout << name2 << " is the same age as " << name1 << "." << std::endl;
 		}
         if (age1>age2) {
-                std::cout << name1 << " is elder than  " << name2 << std::endl;
+                std::cout << name1 << " is elder than  " << name2 << "." << std::endl;
                 }
         if (age1<age2) {
-                std::cout << name2 << " is elder than  " << name1 << std::endl;
+                std::cout << name2 << " is elder than  " << name1 << "." << std::endl;
                 }
-
 	return 0;
 }

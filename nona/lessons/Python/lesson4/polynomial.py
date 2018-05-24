1ca90e5095155dbd7b4d0ecc42433e526682d3d6
Nona1998 <musaielian.nona@mail.ru>
2018-05-25 00:56:12 +0400

    Modified polynomial.py

--- nona/lessons/Python/lesson4/polynomial.py
+++ nona/lessons/Python/lesson4/polynomial.py
@@ -5,10 +5,17 @@ class Polynomial():
         self.a = a
         self.b = b
         self.c = c
-    def solution(self,a,b,c):
-        x1 = (-self.b + (b**2 - 4*self.a*self.c)**0.5)/2*a
-        x2 = (-self.b - (b**2 - 4*self.a*self.c)**0.5)/2*a
-        return "x1 = "+str(x1)+"\nx2 = "+str(x2)
+    def solution(self):
+        d = self.b**2 - 4*self.a*self.c
+        if d > 0:
+            x1 = (-self.b + d**2)/2*self.a
+            x2 = (-self.b - d**2)/2*self.a
+            return "x1 = "+str(x1)+"\nx2 = "+str(x2)
+        elif d == 0:
+            x = -self.b / 2 * self.a
+            return "x = "+str(x)
+        else:
+            return None
     def value(self,x):
         val = self.a*x*x + self.b*x + self.c
         return val
@@ -31,9 +38,10 @@ class Polynomial():
         return str(n)+"x^4 + "+str(m)+"x^3 + "+ str(Polynomial(x,y,z))
     def __str__(self):
         return str(self.a)+"x^2 + "+str(self.b)+"x + "+str(self.c)+" = 0"
-p1 = Polynomial(2,6,17)
+p1 = Polynomial(2,-3,1)
 p2 = Polynomial(1,3,-7)
 print("First polynomial:",p1)
+print(p1.solution())
 print("Second polynomial:",p2)
 print(p1.value(2))
 print("Addition:",p1+p2)

88d57a0d9fbe3535edae2329e2f3a82148433209
Nona1998 <musaielian.nona@mail.ru>
2018-05-24 21:54:53 +0400

    Modified Pythagoras.py

--- nona/lessons/Python/lesson3/Pythagoras.py
+++ nona/lessons/Python/lesson3/Pythagoras.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
 
-trio = []
-for i in range(1,31):
-    for j in range(1,31):
-        trio += [(i,j,k) for k in range(1,31) if i**2 + j**2 == k**2 and i < j]
-print (trio)
+trio = [(i,j,k) for i in range(1,31) for j in range(1,31) for k in range(1,31) if i**2 + j**2 == k**2 and i < j]
+print(trio)
